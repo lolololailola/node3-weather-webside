0e74c5aee288da0bbb471b46cfe16c8a62456bc3
Manuel Ruiz <manuel.ruiz.roble@gmail.com>
2021-03-22 19:02:54 +0100

    Añadir texto en About, modificar Help

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.',
+        helpText: 'Lo siento, no puedo ayudarte en nada.',
         title: 'Help',
         name: 'Lolo Lol'
     })
