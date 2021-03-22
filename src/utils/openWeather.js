b1f5ce394c8beba91ef16b83af9f50f82fedb0f1
Manuel Ruiz <manuel.ruiz.roble@gmail.com>
2021-03-22 19:26:44 +0100

    Añadir datos en openweather.js

--- src/utils/openWeather.js
+++ src/utils/openWeather.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const openWeather = (latitud, longitud, callback) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&units=metric&appid=cfbe300110e0a4233d917e209cb769d5`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&units=metric&lang=es&appid=cfbe300110e0a4233d917e209cb769d5`
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -10,7 +10,9 @@ const openWeather = (latitud, longitud, callback) => {
             callback('Unable to find location!. Try another search', undefined)
         } else {
             callback(undefined, 
-                `Temperatura: ${body.main.temp} , Humedad: ${body.main.humidity}`)
+                `Temperatura: ${body.main.temp}
+                . Humedad: ${body.main.humidity}
+                . Hoy hay ${body.weather[0].description}`)
             }
     })
 }
